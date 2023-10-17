516290f21b4693e19581151e4b089df6b83d62c6
Bishal Pahari <bishal.pahari53@gmail.com>
2023-10-17 10:29:46 +0545

    fix: typos and syntax

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
-import reportWebVitals from "./reportWebVitals";
+import { render } from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 
 import App from "./App";
@@ -8,18 +7,15 @@ import { UserProvider } from "./contexts/user.context";
 
 import "./index.scss";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <UserProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </UserProvider>
-  </React.StrictMode>
-);
+const rootElement = document.getElementById("root");
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <UserProvider>
+        <App />
+      </UserProvider>
+    </BrowserRouter>
+  </React.StrictMode>,
+  rootElement
+);
